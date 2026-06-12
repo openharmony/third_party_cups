605ab5b3d000dde11a5e0a5be6dfde28025ded9e
zhangyong <zhangyong343@huawei.com>
2026-06-12 16:24:36 +0800

    修复溢出
    Signed-off-by: zhangyong <zhangyong343@huawei.com>

--- install.py
+++ install.py
@@ -85,7 +85,8 @@ def do_patch(target_dir):
         "ohos-default-paper-size.patch",
         "ohos-cloud-pagesize-error-fix.patch",
         "cups-log-datamasking.patch",
-        "ohos-reduce-delete-time.patch"
+        "ohos-reduce-delete-time.patch",
+        "ohos-integer-overflow.patch"
     ]
 
     for patch in patch_file:
